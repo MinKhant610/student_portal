bf1492535b3d107273a66606799d9c6e872a6438
MinKhant610 <minkhantiopp@gmail.com>
2024-01-08 21:04:34 +0630

    authorization

--- resources/views/components/subnav.blade.php
+++ resources/views/components/subnav.blade.php
@@ -8,7 +8,8 @@
         <div class="navbar-nav">
           <a class="nav-link text-light" href="/#about">About Our Deparment</a>
           <a class="nav-link text-light" href="/student_protal">For Student</a>
-          <a class="nav-link text-light" href="/login">For Teacher</a>
+          <a class="nav-link text-light" href="/login">
+            {{auth()->user() ? "Dashboard" : "For Teacher" }}</a>
         </div>
       </div>
     </div>
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -4,11 +4,9 @@
         <div class="flex justify-between h-16">
             <div class="flex">
                 <!-- Logo -->
-                <div class="shrink-0 flex items-center">
-                    <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
-                    </a>
-                </div>
+                <a class="navbar-brand" href="/">
+                    <img src="/images/tulogo.png" alt="" class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200 mt-2">
+                </a>
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
